b7a1a7d9c95a586893bb0f6fbd2fc3f8e0e0ce90
ST10303609 <228790302+ST10303609@users.noreply.github.com>
2025-11-20 23:31:52 +0200

    added js and images

---
+++ images/js/script.js
@@ -0,0 +1,21 @@
+
+
+function aboutAlert() {
+                 alert('Welcome to the Woolies About Us Page and these are all the services we have!');
+}
+                
+ function contactAlert() {
+                 alert("Your message has been sent");
+          }
+          
+function faqAlert() {
+               alert("More helpful answers are coming soon!");
+            }
+
+ function indexAlert() {
+    document.getElementById("messageArea").innerHTML =("Welcome to Woolies! Enjoy yor shopping.");  
+ }    
+ 
+ function productsandservicesAlert () {
+          alert("We offer a wide range of Products and Services!");
+            }     
